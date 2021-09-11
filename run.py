507cf9f10bf58d18c29560b1232176c0e318b1d3
Dissy Ulina <dissyulina@gmail.com>
2021-09-11 10:13:27 +0000

    Create respons for user after sending contact form

--- run.py
+++ run.py
@@ -1,14 +1,22 @@
 import os
 import json
-from flask import Flask, render_template #import flask class from flask framework
+# from flask framework, import flask class
+# from flask framework, import render_template to create a base template for html
+# from flask framework, import request function to create backend for the contact form
+# from flask framework, import flash function to provide feedback to the user
+from flask import Flask, render_template, request, flash
+# import env if the system can find an env.py file
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__) #create an instance of this and store it in a variable called app. The argument is the name of the application's module - our package.
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/") #a decorator (pie-notation). we try to brouwse the root directory.
 def index():
-    return render_template("index.html")
+    return render_template("index.html") 
 
 
 @app.route("/about")
@@ -28,8 +36,11 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get('name'))) # by using get method, if there's no name key, it would return None
     return render_template("contact.html", page_title="Contact")
 
 
